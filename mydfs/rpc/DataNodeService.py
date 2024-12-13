0431199d3e8b035ff6c7ba6c1bb4c14d852a7427
luizfernandofo <luizfernandofo@hotmail.com>
2024-12-13 16:09:48 -0300

    Refactor: Implementação inicial de ClusterManager e DataNode usando Pyro5.

--- mydfs/rpc/DataNodeService.py
+++ mydfs/rpc/DataNodeService.py
@@ -1,20 +1,58 @@
-import rpyc
+import os
+import sys
+import uuid
+import threading
+import time
+import Pyro5.api
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from mydfs.utils.get_proxy_by_name import get_proxy_by_name
+from mydfs.models.DataNode.Shard import Shard
+
+INTERVAL_SHARD_REPORT = 10
+
+if os.path.exists('data_node_config.txt'):
+  with open('data_node_config.txt', 'r') as f:
+    TOKEN = f.read().strip()
+else:
+  TOKEN = str(uuid.uuid4())
+  with open('data_node_config.txt', 'w') as f:
+    f.write(TOKEN)
 
 
-class DataNodeService(rpyc.Service):
-    def __init__(self):
-        super().__init__()
+@Pyro5.api.expose
+class DataNodeService:
+  def __init__(self):
+    print(f"DataNode {TOKEN} started")
+    self.TOKEN = TOKEN
+    self.__cluster_manager_proxy = get_proxy_by_name("cluster-manager")
+    self.__shards : list[Shard] = [Shard(file_name=f"shard_{i}.dat", file_path="/tmp", shard_size=10) for i in range(2)]
+      
+    try:
+      self.__keep_running = True
+      self.__report_thread = threading.Thread(target=self.__report_shards_periodically)
+      self.__report_thread.start()
+    except Exception as e:
+      print(f"Failed to connect to cluster manager: {e}")
 
-    def on_connect(self, conn):
-        print("Connection established")
-
-    def on_disconnect(self, conn):
-        print("Connection closed")
+  def __report_shards_periodically(self):
+    cluster_manager_proxy = get_proxy_by_name("cluster-manager")
+    while self.__keep_running:
+      try:
+        cluster_manager_proxy.report_shards(self.TOKEN, [shard.file_name for shard in self.__shards])
+      except Exception as e:
+        print(f"Failed to report shards: {e}")
+      time.sleep(INTERVAL_SHARD_REPORT)
 
 
 if __name__ == "__main__":
-    # connect to the cluster manager
-    token = "1234"
-    conn = rpyc.connect("localhost", 25565)
-    conn.root.register_data_node(token)
-    conn.root.report_shards(token)
+  with Pyro5.api.Daemon() as daemon:
+    uri = daemon.register(DataNodeService(), f"dn-{TOKEN}")
+    try:
+      ns = Pyro5.api.locate_ns()
+      ns.register(f"dn-{TOKEN}", uri)
+    except Exception as e:
+      print(f"Failed to locate nameserver: {e}")
+      exit(1)
+    print("DataNode Service started")
+    daemon.requestLoop()
