b8dd9d031a33433ebf4692756dbf9dd45228bb0b
luizfernandofo <luizfernandofo@hotmail.com>
2024-12-14 17:18:51 -0300

    Fix: Correção de função que deletava item de dict enquanto iterava e correção em comparação de tempo.

--- mydfs/models/ClusterManager/DataNodesConnected.py
+++ mydfs/models/ClusterManager/DataNodesConnected.py
@@ -10,14 +10,14 @@ from mydfs.utils.lock_decorator import synchronized
 class DataNodesConnected:
   def __init__(self):
     self.__data_nodes: dict[str, DataNodeVitals] = {}
-    self.__LAST_UPDATE_THRESHOLD = 45 # seconds
+    self.__LAST_UPDATE_THRESHOLD = 45 # nanoseconds
     self.__CPU_USAGE_THRESHOLD = 70 # percent
     self.__MINIUM_SHARD_TO_STORE = 8
 
   def __remove_dead_data_nodes(self):
-    for token, data_node in self.__data_nodes.items():
-      if data_node.time_since_last_update() > self.__LAST_UPDATE_THRESHOLD:
-        del self.__data_nodes[token]
+    tokens_to_remove = [token for token, data_node in self.__data_nodes.items() if data_node.time_since_last_update() > self.__LAST_UPDATE_THRESHOLD * 1e9]
+    for token in tokens_to_remove:
+      del self.__data_nodes[token]
 
   @synchronized
   def update_data_node_vitals(self, token: str, vitals: dict):
@@ -26,7 +26,7 @@ class DataNodesConnected:
     else:
       self.__data_nodes[token].update_vitals(vitals['cpu_usage'], vitals['ram_available'], vitals['disk_available'])
 
-    self.__remove_dead_data_nodes()    
+    self.__remove_dead_data_nodes()
 
   @synchronized
   def any_data_node_connected(self) -> bool:
@@ -42,6 +42,10 @@ class DataNodesConnected:
           and data_node.__cpu_usage <= self.__CPU_USAGE_THRESHOLD
           and data_node.can_store_n_shards(self.__MINIUM_SHARD_TO_STORE)]
     return dn
+
+  @synchronized
+  def get_data_nodes(self) -> dict[str, DataNodeVitals]:
+    return self.__data_nodes
 
   def __str__(self):
     return serpent.dumps(self.__data_nodes).decode('utf-8')    
