9715ada5f34dc5d932d1de7ba88c9d452655f567
luizfernandofo <luizfernandofo@hotmail.com>
2024-12-14 16:13:21 -0300

    Feat: Inclusãó de __getstate__ e __setstate__ em Response para poder ser serializada.

--- mydfs/models/Reponse.py
+++ mydfs/models/Reponse.py
@@ -1,16 +1,31 @@
 class Response:
 
-  class Status:
-    OK = 200
-    FILE_ALREADY_EXISTS = 400
-    NO_DATA_NODES = 501
-    DATA_NODES_ARE_BUSY = 502
+    class Status:
+        OK = 200
+        FILE_ALREADY_EXISTS = 400
+        NO_DATA_NODES = 501
+        DATA_NODES_ARE_BUSY = 502
 
-  class Body:
-    def __init__(self, msg: str, data: dict = None):
-      self.msg = msg
-      self.data = data
+    class Body:
+        def __init__(self, msg: str, data: dict = None):
+            self.msg = msg
+            self.data = data
 
-  def __init__(self, status_code: int, body: Body):
-    self.status_code = status_code
-    self.body = body
+        def __getstate__(self):
+            return self.__dict__
+
+        def __setstate__(self, state):
+            self.__dict__.update(state)
+
+    def __init__(self, status_code: int, body: Body):
+        self.status_code = status_code
+        self.body = body
+
+    def __getstate__(self):
+        return self.__dict__
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+
+    def from_dict(d: dict):
+        return Response(d['status_code'], Response.Body(d['body']['msg'], d['body']['data']))
