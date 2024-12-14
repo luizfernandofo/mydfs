927a2b6d5b1465e9fd401a8554ff8130dddcfd8f
luizfernandofo <luizfernandofo@hotmail.com>
2024-12-14 15:20:08 -0300

    Feat: Criação de classe Response.

---
+++ mydfs/models/Reponse.py
@@ -0,0 +1,15 @@
+class Response:
+
+  class Status:
+    OK = 200
+    NO_DATA_NODES = 501
+    DATA_NODES_ARE_BUSY = 502
+
+  class Body:
+    def __init__(self, msg: str, data: dict = None):
+      self.msg = msg
+      self.data = data
+
+  def __init__(self, status_code: int, body: Body):
+    self.status_code = status_code
+    self.body = body
