e389300511ff9dea16651cb52f318ef11403cf35
luizfernandofo <luizfernandofo@hotmail.com>
2024-12-14 15:21:41 -0300

    Feat: Criações de funções para controlar os nós conectados no cluster.

--- mydfs/models/ClusterManager/DataNodesConnected.py
+++ mydfs/models/ClusterManager/DataNodesConnected.py
@@ -1,5 +1,6 @@
 from multiprocessing import synchronize
 import sys, os, serpent
+import threading
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from mydfs.models.ClusterManager.DataNodeVitals import DataNodeVitals
@@ -8,7 +9,15 @@ from mydfs.utils.lock_decorator import synchronized
 
 class DataNodesConnected:
   def __init__(self):
-    self.__data_nodes: dict[DataNodeVitals] = {}
+    self.__data_nodes: dict[str, DataNodeVitals] = {}
+    self.__LAST_UPDATE_THRESHOLD = 45 # seconds
+    self.__CPU_USAGE_THRESHOLD = 70 # percent
+    self.__MINIUM_SHARD_TO_STORE = 8
+
+  def __remove_dead_data_nodes(self):
+    for token, data_node in self.__data_nodes.items():
+      if data_node.time_since_last_update() > self.__LAST_UPDATE_THRESHOLD:
+        del self.__data_nodes[token]
 
   @synchronized
   def update_data_node_vitals(self, token: str, vitals: dict):
@@ -17,6 +26,22 @@ class DataNodesConnected:
     else:
       self.__data_nodes[token].update_vitals(vitals['cpu_usage'], vitals['ram_available'], vitals['disk_available'])
 
-  def __str__(self):
-    return serpent.dumps(self.__data_nodes).decode('utf-8')
+    self.__remove_dead_data_nodes()    
+
+  @synchronized
+  def any_data_node_connected(self) -> bool:
+    return len(self.__data_nodes) > 0
+
+  @synchronized
+  def get_suitable_data_nodes_for_upload(self) -> list[str]:
+    if len(self.__data_nodes) == 1:
+      return list(self.__data_nodes.keys())
     
+    dn = [token for token, data_node in self.__data_nodes.items() if
+          data_node.time_since_last_update() < self.__LAST_UPDATE_THRESHOLD
+          and data_node.__cpu_usage <= self.__CPU_USAGE_THRESHOLD
+          and data_node.can_store_n_shards(self.__MINIUM_SHARD_TO_STORE)]
+    return dn
+
+  def __str__(self):
+    return serpent.dumps(self.__data_nodes).decode('utf-8')    
