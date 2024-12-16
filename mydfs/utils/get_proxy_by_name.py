7750c1f8640537f0bf64d17856e6859aafdd19e5
luizfernandofo <luizfernandofo@hotmail.com>
2024-12-16 02:41:25 -0300

    Refactor: Melhoria no tratamento de erro da função utilitária get_proxy_by_name.

--- mydfs/utils/get_proxy_by_name.py
+++ mydfs/utils/get_proxy_by_name.py
@@ -2,8 +2,15 @@ import Pyro5.api
 
 def get_proxy_by_name(name_service: str) -> Pyro5.api.Proxy:
   try:
-    ns = Pyro5.api.locate_ns()
-    return Pyro5.api.Proxy(ns.lookup(name_service))
+    ns = Pyro5.api.locate_ns()    
   except Exception as e:
-    print(f"Failed to locate nameserver: {e}")
-    exit(1)
+    e.add_note("Failed to locate nameserver")
+    raise e
+  else:
+    try:
+      uri = ns.lookup(name_service)
+    except Exception as e:
+      e.add_note(f"Failed to lookup service {name_service}")
+      raise e
+    else:
+      return Pyro5.api.Proxy(uri)
